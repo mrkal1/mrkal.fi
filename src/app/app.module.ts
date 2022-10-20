ea03af3074dd417505b08a33056128b35b5c1185
Mr. Kal <kalkkuna05@gmail.com>
2022-10-20 23:00:17 +0300

    IDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgParticlesModule 
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
